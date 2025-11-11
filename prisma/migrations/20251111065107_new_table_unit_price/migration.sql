cf7a12591a5481c0dcc71d8e05fa70ae6e691910
Panumas Srisook <Panumas.srisook@gmail.com>
2025-11-11 13:56:47 +0700

    update table unitPrice

---
+++ prisma/migrations/20251111065107_new_table_unit_price/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `unitPrice` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `value` VARCHAR(191) NOT NULL,
+    `label` VARCHAR(191) NOT NULL,
+    `flag` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
