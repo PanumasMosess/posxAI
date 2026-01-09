2859cbde84b3efa0160f4ea7003c95d0c500189c
Panumas Srisook <Panumas.srisook@gmail.com>
2026-01-09 15:50:53 +0700

    layout

--- src/components/home/MainPageComponentsHome.tsx
+++ src/components/home/MainPageComponentsHome.tsx
@@ -81,20 +81,8 @@ const MainPageComponentsHome = ({
           />
         )}
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        {/* <CardList title="Latest Transactions" /> */}
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        {/* <AppPieChart /> */}
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        <TodoList />
-      </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        <AppAreaChart />
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">
-        <CardList title="Popular Content" />
+        {/* <CardList title="Latest Transactions" /> */}
       </div>
     </div>
   );
