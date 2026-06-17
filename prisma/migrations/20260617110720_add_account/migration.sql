689d0f7b803e6f18d514e144ab2ae1e92310f12e
nuttapong <nuttaponmg.ep@gmail.com>
2026-06-17 18:08:13 +0700

    edit db account

--- prisma/migrations/20260617110720_add_account/migration.sql
+++ prisma/migrations/20260617110720_add_account/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE `account` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `organizationId` INTEGER NOT NULL,
+    `organizationId` INTEGER NULL,
     `accountName` VARCHAR(191) NOT NULL,
     `balance` DOUBLE NOT NULL DEFAULT 0.00,
     `status` VARCHAR(191) NOT NULL DEFAULT 'ACTIVE',
@@ -49,7 +49,7 @@ CREATE TABLE `account_transaction` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `account` ADD CONSTRAINT `account_organizationId_fkey` FOREIGN KEY (`organizationId`) REFERENCES `organization`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `account` ADD CONSTRAINT `account_organizationId_fkey` FOREIGN KEY (`organizationId`) REFERENCES `organization`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `account_category` ADD CONSTRAINT `account_category_organizationId_fkey` FOREIGN KEY (`organizationId`) REFERENCES `organization`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
